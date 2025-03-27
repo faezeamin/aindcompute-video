ddc0b17d19be29fac4da6e7dbd65f1e38dde8061
Faeze Aminmansoor <faeze.aminmansoor@aindcompute-ux1.corp.alleninstitute.org>
2025-03-26 18:36:40 -0700

    Initial commit

---
+++ scratch/706893_2024-05-28_15-15-38/remaining_preparation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# mkdir -p /root/capsule/scratch/xinxin_706893_2024-05-28/results
+# mkdir -p /root/capsule/scratch/xinxin_706893_2024-05-28/figures
+
+cp -r /root/capsule/scratch/configs_xinxin_face_50Hz/ae_jsons/* ~/.behavenet
+
+#  - 684039_2023-12-13_10-46-46 : 5e8e0425-b845-4172-9c3c-e5fd27deea8b :: data asset: xinxin_video_split_10sec_face_50Hz_hdf5
+#  - 706893_2024-04-18_16-28-57 : a1357d16-fd42-482c-989c-d43678b60a84 :: data asset: behavior_706893_2024-04-18_16-28-57_video-side-left-face-50fr-10sec-clips_hdf5
